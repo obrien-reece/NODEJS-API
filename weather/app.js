199b04e18592ffb26c6aa443772e902477bddb87
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-09 00:00:25 +0300

    Am learning NodeJS API and HTTP requests. This is my tutorial folder

--- weather/app.js
+++ weather/app.js
@@ -5,13 +5,17 @@ const chalk = require('chalk')
 // for scientific
 // const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=s'
 // for farhenheit
-const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=f'
-for metric 
-
-
+// const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=f'
+// for metric 
 const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233'
 
 request({ url:url, json:true }, (error, response) => {
+    if(error){
+
+        console.log("Unable to connect to weather services");
+
+    }else{
+
     // const data = JSON.parse(response.body)
     // console.log(response.body.current);
     console.log(chalk.green("Its ") +
@@ -20,6 +24,8 @@ request({ url:url, json:true }, (error, response) => {
                chalk.red(response.body.current.temperature) + 
                " degrees. It feels like " + 
                chalk.red(response.body.current.feelslike) + " degrees"));
+
+    }
 })
 
 // const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGhhdGJveXJlZWNlIiwiYSI6ImNrcG9oYmloYjAyd3AydnFxNjBxaTJoNDgifQ.yfAdMhYBtriEhHIeak5j4A&limit=1"
