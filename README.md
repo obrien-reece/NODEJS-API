9034495ad67cff1444fab5b10bbef03f22c1f8cd
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-08 23:55:56 +0300

    Am learning NodeJS API and HTTP requests. This is my tutorial folder

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NODEJS-API
+# At the moment am studying NodeJS with andrewmead as my udemy teacher
+# API - Applicarion Programming Interface
+# Am developing a weather applications using two APIs
+# Weatherstack api (weatherstack.com) and mapbox API (mapbox.com)
--- weather/app.js
+++ weather/app.js
@@ -1,13 +1,13 @@
 const request = require("request")
 const chalk = require('chalk')
 
-/*
-for scientific
-const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=s'
-for farhenheit
+
+// for scientific
+// const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=s'
+// for farhenheit
 const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233&units=f'
 for metric 
-*/
+
 
 const url = 'http://api.weatherstack.com/current?access_key=13b5e5c13be19948d516ac2c69dead86&query=37.8267,122.4233'
 
@@ -21,3 +21,13 @@ request({ url:url, json:true }, (error, response) => {
                " degrees. It feels like " + 
                chalk.red(response.body.current.feelslike) + " degrees"));
 })
+
+// const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGhhdGJveXJlZWNlIiwiYSI6ImNrcG9oYmloYjAyd3AydnFxNjBxaTJoNDgifQ.yfAdMhYBtriEhHIeak5j4A&limit=1"
+
+// request({ url:url, json:true }, (error, response) => {
+//     const longitude = response.body.features[0].center[0]
+//     console.log(longitude);
+//     const latitude = response.body.features[0].center[1]
+//     console.log(latitude);    
+//     // console.log(latitude, longitude);
+// })
