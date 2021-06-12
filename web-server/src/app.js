59fa735d6c6ae69c87cd69f43b5ba6df1b6f4409
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-12 21:27:21 +0300

    Serving up JSON and HTML code to my server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,29 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send("Hello Express!")
+    res.send("<h1>Weather</h1>")
 })
 
 app.get('/help', (req, res) => {
-    res.send("Help Page")
+    res.send([{
+        name: `Evance O'brien`,
+        skillset: `Dark Arts`
+    },{
+        name: `Sandrah Agizah`,
+        skillset: `Assasins`
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send("This is the about page")
+    res.send("<h1><strong>This is the about page title</strong></h1>")
 })
 
 app.get('/weather', (req, res) => {
-    res.send("This is the weather page")
+    res.send([{
+        forecast: `It's partly cloudy at the moment`
+    },{
+        location: `Nairobi Kenya`
+    }])
 })
 
 app.listen(3000, () => {
