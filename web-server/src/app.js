30147292ad75af0255e15ff6308788428009f762
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-13 11:40:15 +0300

    AM using handlebars to serve up dynamic web pages. I corrected yesterdays error by adding the app.use(set directory to views) in the app.js.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,34 @@
 const path = require("path")
 const express = require('express')
+const app = express()
+
 
 console.log(__dirname);
 console.log(path.join(__dirname, "../public"));
 
-const app = express()
-
 const publicDirectoryPath = path.join(__dirname, "../public")
-
-app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+//set the directory for the views
+app.set('views', path.join(__dirname, "../views"))
+app.set('view engine', 'hbs')
+
+//using the handlebars templating to serve up dynamic pages
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: "My name is: ",
+        name: `Evance O'Brien`
+    })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        titleabout: "This is the about page",
+        nameabout: "Evance O'Brien"
+    })
+})
+
+//serving up static pages that do not require changing
 app.get('/help', (req, res) => {
     res.send([{
         name: `Evance O'brien`,
@@ -25,10 +39,12 @@ app.get('/help', (req, res) => {
     }])
 })
 
+//serving up static pages that do not require changing
 app.get('/about', (req, res) => {
     res.send("<h1><strong>This is the about page title</strong></h1>")
 })
 
+//serving up static pages that do not require changing
 app.get('/weather', (req, res) => {
     res.send([{
         forecast: `It's partly cloudy at the moment`
