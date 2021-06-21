a88de764ebc10ac33455c60a2827957dfbfba4ab
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-21 21:46:42 +0300

    Async Await Procedures

--- task-manager/tests/4-async-await.js
+++ task-manager/tests/4-async-await.js
@@ -1,6 +1,9 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if(a < 0 || b < 0){
+                return reject("Numbers must be none negative")
+            }
             resolve(a + b)    
         }, 2000);
         
@@ -9,7 +12,7 @@ const add = (a, b) => {
 
 
 const doWork = async () => {
-    const sum = await add(3, 5)
+    const sum = await add(3, -5)
     const sum1 = await add(sum, 5)
     const sum2 = await add(sum1, 5)
     const sum3 = await add(sum2, 5)
