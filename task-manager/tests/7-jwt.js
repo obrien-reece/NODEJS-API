1619bc212580a7783ab65b4d17a6e03ee945c3a4
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-22 22:57:20 +0300

    JSON web tokens example

--- task-manager/tests/7-jwt.js
+++ task-manager/tests/7-jwt.js
@@ -1,5 +1,8 @@
 const jwt = require("jsonwebtoken")
 
+
+
+
 const token = jwt.sign({ _id: "2344323443" }, "thisismynewtoken", { expiresIn: "2 seconds" })
 
 const data = jwt.verify(token, "thisismynewtoken")
