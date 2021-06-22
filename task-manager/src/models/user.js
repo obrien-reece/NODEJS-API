b0e91957223da9467987633172023a150b646699
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-22 13:19:21 +0300

    Added the login aunthentication part I. Now the user with the correct email and password is the only one who can now login to the system

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value){
@@ -41,6 +42,23 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+        throw new Error("Unable to login")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch){
+
+        throw new Error("Unable to login")
+    }
+
+    return user
+}
 
 userSchema.pre('save', async function (next) {
     const user = this
