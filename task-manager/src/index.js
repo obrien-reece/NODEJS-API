65a9f93ab11f606098ddb6049b19c043e82fa10d
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-23 16:07:45 +0300

    Aunthentication middleware testrun

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require("./routers/task")
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     console.log(req.method, req.path);
+//     next()
+// })
+
+app.use((req, res, next) => {
+    if(req.method === 'GET' || 'POST' || 'PATCH', 'DELETE') {
+        res.status(503).send("Server is under maintanance")
+    }
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
