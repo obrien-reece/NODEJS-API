c25fefbb75f4aaff8f9316ef35f4d0eb8eb72fc9
OBrien-reece <indeche.evans.o@gmail.com>
2021-06-30 11:25:55 +0300

    Added the SHARP npm library to resize the image uploaded according to my needs

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const multer = require('multer')
+const sharp = require("sharp")
 const User = require('../models/user')
 const auth = require("../middleware/auth")
 const router = new express.Router()
@@ -124,7 +125,10 @@ const upload = multer({
 })
 
 router.post("/users/me/avatar", auth,  upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
 
     await req.user.save()
     res.send()
@@ -148,7 +152,7 @@ try{
         throw new Error()
     }
 
-    res.set("Content-Type", "image/jpg")
+    res.set("Content-Type", "image/png")
     res.send(user.avatar)
 }catch(e) {
     res.send(e)
